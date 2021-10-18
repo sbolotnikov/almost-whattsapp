c3a9f66e58f1c4bc83c81954fcac6165e4523016
Sergey Bolotnikov <sbolotnikov@gmail.com>
2021-10-18 13:11:02 -0400

    fix wrong API error

--- firebase.js
+++ firebase.js
@@ -4,15 +4,17 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
+
+
 const firebaseConfig = {
   apiKey: process.env.CHAT_FIREBASE_APIKEY,
-  authDomain: process.env.CHAT_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.CHAT_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.CHAT_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.CHAT_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.CHAT_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC5VrkrvUr2zl3sVnOhqk9ywsRya60HcFQ",
+  authDomain: "almost-whattsup.firebaseapp.com",
+  projectId: "almost-whattsup",
+  storageBucket: "almost-whattsup.appspot.com",
+  messagingSenderId: "946261875744",
+  appId: "1:946261875744:web:d410a9319fe1bf7703f045"
 };
-
 const app = !firebase.apps.length
   ? firebase.initializeApp(firebaseConfig)
   : firebase.app();
