58e45540d9ca5858b7ae12417d736b2e6aebb215
Sergey Bolotnikov <sbolotnikov@gmail.com>
2021-10-18 11:50:13 -0400

    finish Chat, ChatScreen, Message components

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -9,31 +9,37 @@ import { auth, db } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollection } from "react-firebase-hooks/firestore";
 // import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
+import "firebase/compat/firestore";
 
 function Sidebar() {
-    const [user] = useAuthState(auth);
-    const userChatRef = db.collection('chats').where('users', 'array-contains', user.email);
-    const [chatsSnapshot] = useCollection(userChatRef);
-    const createChat =() => {
-        const input = prompt(
-            "Please enter an email address for the user you wish to chat with"
-            );
-        if (!input) return null;  
-        if (EmailValidator.validate(input) && !chatAlreadyExists(input) && input!==user.email) {
-            // add chat to db
-            db.collection('chats').add({
-              users: [user.email, input],
-            })
-        }
+  const [user] = useAuthState(auth);
+  const userChatRef = db
+    .collection("chats")
+    .where("users", "array-contains", user.email);
+  const [chatsSnapshot] = useCollection(userChatRef);
+  const createChat = () => {
+    const input = prompt(
+      "Please enter an email address for the user you wish to chat with"
+    );
+    if (!input) return null;
+    if (
+      EmailValidator.validate(input) &&
+      !chatAlreadyExists(input) &&
+      input !== user.email
+    ) {
+      // add chat to db
+      db.collection("chats").add({
+        users: [user.email, input],
+      });
     }
+  };
 
-    const chatAlreadyExists = (recipientEmail) =>
-       !!chatsSnapshot?.docs.find(
-         (chat) =>
-            chat.data().users.find((user) => user === recipientEmail)?.length >0
-            );
-    
+  const chatAlreadyExists = (recipientEmail) =>
+    !!chatsSnapshot?.docs.find(
+      (chat) =>
+        chat.data().users.find((user) => user === recipientEmail)?.length > 0
+    );
+
   return (
     <Container>
       <Header>
@@ -63,7 +69,20 @@ function Sidebar() {
 
 export default Sidebar;
 
-const Container = styled.div``;
+const Container = styled.div`
+  flex: 0.45;
+  border-right: 1px solid whitesmoke;
+  height: 100vh;
+  min-width: 300px;
+  max-width: 350px;
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  -ms-overflow-style: none; /*IE and Edge*/
+  scrollbar-width: none; /* Firefox */
+`;
 
 const Search = styled.div`
   display: flex;
@@ -92,7 +111,7 @@ const Header = styled.div`
   z-index: 1;
   justify-content: space-between;
   align-items: center;
-  padding: 15px;
+  padding: 11px;
   height: 80px;
   border-bottom: 1px solid whitesmoke;
 `;
--- firebase.js
+++ firebase.js
@@ -1,24 +1,23 @@
-import { initializeApp, getApps, getApp } from 'firebase/app';
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth, GoogleAuthProvider  } from "firebase/auth";
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
 import "firebase/compat/auth";
-  const firebaseConfig = {
-    apiKey: process.env.CHAT_FIREBASE_APIKEY,
-    authDomain: process.env.CHAT_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.CHAT_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.CHAT_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.CHAT_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.CHAT_FIREBASE_APP_ID
-  };
+const firebaseConfig = {
+  apiKey: process.env.CHAT_FIREBASE_APIKEY,
+  authDomain: process.env.CHAT_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.CHAT_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.CHAT_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.CHAT_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.CHAT_FIREBASE_APP_ID,
+};
 
-  console.log( firebaseConfig);
-  const app = !firebase.apps.length
+const app = !firebase.apps.length
   ? firebase.initializeApp(firebaseConfig)
   : firebase.app();
-  const db = app.firestore();
-  const auth = app.auth();
-  const provider = new firebase.auth.GoogleAuthProvider();
+const db = app.firestore();
+const auth = app.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
 
-  export { db, auth, provider };
+export { db, auth, provider };
